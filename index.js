de711517790cf1111592b8a9be258ccfa1861814
Emil <catar.emil49@gmail.com>
2018-10-21 15:36:43 +0300

    add express

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
-console.log('tests')
+const express = require('express');
 
-console.log('tests12')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.get('/test', (req, res) => res.send('Hello World! test'))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
